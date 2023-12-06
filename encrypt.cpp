7326b8afb6acdf69e49b341ca358290e45ca5603
Marco Magdy <m_magdy@apple.com>
2023-12-05 18:13:09 -0800

    use hex encode for everything
    Because they work well on the command line

--- encrypt.cpp
+++ encrypt.cpp
@@ -8,38 +8,38 @@
 
 #include <string>
 
-std::string base64_encode(unsigned char* bytes, size_t length)
-{
-    BIO *bio, *b64;
-    BUF_MEM *bufferPtr;
+std::string base64_encode(unsigned char *bytes, size_t length) {
+  BIO *bio, *b64;
+  BUF_MEM *bufferPtr;
 
-    b64 = BIO_new(BIO_f_base64());
-    bio = BIO_new(BIO_s_mem());
-    bio = BIO_push(b64, bio);
+  b64 = BIO_new(BIO_f_base64());
+  bio = BIO_new(BIO_s_mem());
+  bio = BIO_push(b64, bio);
 
-    BIO_write(bio, bytes, length);
-    BIO_flush(bio);
-    BIO_get_mem_ptr(bio, &bufferPtr);
+  BIO_write(bio, bytes, length);
+  BIO_flush(bio);
+  BIO_get_mem_ptr(bio, &bufferPtr);
 
-    std::string result(bufferPtr->data, bufferPtr->length);
+  std::string result(bufferPtr->data, bufferPtr->length);
 
-    BIO_free_all(bio);
+  BIO_free_all(bio);
 
-    return result;
+  return result;
 }
 
-std::string hex_encode(unsigned char* bytes, size_t length)
-{
-    std::string result;
-    for (size_t i = 0; i < length; i++) {
-        char buf[3];
-        sprintf(buf, "%02x", bytes[i]);
-        result += buf;
-    }
-    return result;
+std::string hex_encode(unsigned char *bytes, size_t length) {
+  std::string result;
+  const auto hex_digits = "0123456789ABCDEF";
+  result.resize(length * 2);
+  for (size_t i = 0; i < length; i++) {
+    result[2 * i] = hex_digits[(bytes[i] & 0xF0) >> 4];
+    result[2 * i + 1] = hex_digits[bytes[i] & 0x0F];
+  }
+  return result;
 }
 
-bool encrypt_with_public_key(EVP_PKEY *publicKey, const std::string &message, std::string &encrypted_message) {
+bool encrypt_with_public_key(EVP_PKEY *publicKey, const std::string &message,
+                             std::string &encrypted_message) {
   EVP_PKEY_CTX *ctx;
   size_t outlen;
   unsigned char *outbuf;
@@ -54,14 +54,16 @@ bool encrypt_with_public_key(EVP_PKEY *publicKey, const std::string &message, st
   if (EVP_PKEY_CTX_set_rsa_padding(ctx, RSA_PKCS1_PADDING) <= 0)
     return false;
 
-  if (EVP_PKEY_encrypt(ctx, NULL, &outlen, (unsigned char *)message.c_str(), message.size()) <= 0)
+  if (EVP_PKEY_encrypt(ctx, NULL, &outlen, (unsigned char *)message.c_str(),
+                       message.size()) <= 0)
     return false;
 
   outbuf = (unsigned char *)malloc(outlen);
   if (!outbuf)
     return false;
 
-  if (EVP_PKEY_encrypt(ctx, outbuf, &outlen, (unsigned char *)message.c_str(), message.length()) <= 0)
+  if (EVP_PKEY_encrypt(ctx, outbuf, &outlen, (unsigned char *)message.c_str(),
+                       message.length()) <= 0)
     return false;
 
   // encrypted_message = base64_encode(outbuf, outlen);
@@ -73,34 +75,33 @@ bool encrypt_with_public_key(EVP_PKEY *publicKey, const std::string &message, st
   return true;
 }
 
-int main(int argc, char **argv)
-{
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <public key path> <message>\n", argv[0]);
-        return EXIT_FAILURE;
-    }
+int main(int argc, char **argv) {
+  if (argc != 3) {
+    fprintf(stderr, "Usage: %s <public key path> <message>\n", argv[0]);
+    return EXIT_FAILURE;
+  }
 
-    const char *public_key_path = argv[1];
-    const char *message = argv[2];
+  const char *public_key_path = argv[1];
+  const char *message = argv[2];
 
-    FILE *public_key_file = fopen(public_key_path, "r");
-    if (!public_key_file) {
-        perror("Error opening private key file");
-        return 1;
-    }
+  FILE *public_key_file = fopen(public_key_path, "r");
+  if (!public_key_file) {
+    perror("Error opening private key file");
+    return 1;
+  }
 
-    EVP_PKEY* public_key = PEM_read_PUBKEY(public_key_file, NULL, NULL, NULL);
-    if (!public_key) {
-        fprintf(stderr, "Error reading public key\n");
-        return 1;
-    }
+  EVP_PKEY *public_key = PEM_read_PUBKEY(public_key_file, NULL, NULL, NULL);
+  if (!public_key) {
+    fprintf(stderr, "Error reading public key\n");
+    return 1;
+  }
 
-    std::string encrypted_message;
-    if (!encrypt_with_public_key(public_key, message, encrypted_message)) {
-        fprintf(stderr, "Error encrypting message\n");
-        return 1;
-    }
+  std::string encrypted_message;
+  if (!encrypt_with_public_key(public_key, message, encrypted_message)) {
+    fprintf(stderr, "Error encrypting message\n");
+    return 1;
+  }
 
-    printf("Encrypted message: %s\n", encrypted_message.c_str());
-    return 0;
+  printf("Encrypted message: %s\n", encrypted_message.c_str());
+  return 0;
 }
--- sign.cpp
+++ sign.cpp
@@ -1,115 +1,126 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <openssl/bio.h>
 #include <openssl/evp.h>
 #include <openssl/pem.h>
 #include <openssl/rand.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include <string>
 
 void handleErrors() {
-    fprintf(stderr, "Error occurred.\n");
-    exit(EXIT_FAILURE);
+  fprintf(stderr, "Error occurred.\n");
+  exit(EXIT_FAILURE);
 }
-std::string base64_encode(unsigned char* bytes, size_t length);
+std::string base64_encode(unsigned char *bytes, size_t length);
+
+std::string hex_encode(unsigned char *bytes, size_t length) {
+  std::string result;
+  const auto hex_digits = "0123456789ABCDEF";
+  result.resize(length * 2);
+  for (size_t i = 0; i < length; i++) {
+    result[2 * i] = hex_digits[(bytes[i] & 0xF0) >> 4];
+    result[2 * i + 1] = hex_digits[bytes[i] & 0x0F];
+  }
+  return result;
+}
 
 int signMessage(const char *message, const char *privateKeyPath) {
-    FILE *privateKeyFile = fopen(privateKeyPath, "r");
-    if (!privateKeyFile) {
-        perror("Error opening private key file");
-        return 1;
-    }
+  FILE *privateKeyFile = fopen(privateKeyPath, "r");
+  if (!privateKeyFile) {
+    perror("Error opening private key file");
+    return 1;
+  }
 
-    // The key must be in PEM format. Notice that OpenSSH uses a different pem format.
-    // You can convert an OpenSSH key to PEM with the following command:
-    // ssh-keygen -p -m PEM -f ~/.ssh/id_rsa
-    // The same goes for public keys.
-    EVP_PKEY *privateKey = PEM_read_PrivateKey(privateKeyFile, NULL, NULL, NULL);
-    fclose(privateKeyFile);
+  // The key must be in PEM format. Notice that OpenSSH uses a different pem
+  // format. You can convert an OpenSSH key to PEM with the following command:
+  // ssh-keygen -p -m PEM -f ~/.ssh/id_rsa
+  // The same goes for public keys.
+  EVP_PKEY *privateKey = PEM_read_PrivateKey(privateKeyFile, NULL, NULL, NULL);
+  fclose(privateKeyFile);
 
-    if (!privateKey) {
-        fprintf(stderr, "Error reading private key\n");
-        return 1;
-    }
+  if (!privateKey) {
+    fprintf(stderr, "Error reading private key\n");
+    return 1;
+  }
 
-    // Create a signature context
-    EVP_MD_CTX *ctx = EVP_MD_CTX_create();
-    if (!ctx) {
-        handleErrors();
-    }
+  // Create a signature context
+  EVP_MD_CTX *ctx = EVP_MD_CTX_create();
+  if (!ctx) {
+    handleErrors();
+  }
 
-    // Initialize the signing operation
-    if (EVP_DigestSignInit(ctx, NULL, EVP_sha256(), NULL, privateKey) != 1) {
-        handleErrors();
-    }
+  // Initialize the signing operation
+  if (EVP_DigestSignInit(ctx, NULL, EVP_sha256(), NULL, privateKey) != 1) {
+    handleErrors();
+  }
 
-    // Update the message to be signed
-    if (EVP_DigestSignUpdate(ctx, message, strlen(message)) != 1) {
-        handleErrors();
-    }
+  // Update the message to be signed
+  if (EVP_DigestSignUpdate(ctx, message, strlen(message)) != 1) {
+    handleErrors();
+  }
 
-    // Finalize the signature
-    size_t signatureLen;
-    if (EVP_DigestSignFinal(ctx, NULL, &signatureLen) != 1) {
-        handleErrors();
-    }
+  // Finalize the signature
+  size_t signatureLen;
+  if (EVP_DigestSignFinal(ctx, NULL, &signatureLen) != 1) {
+    handleErrors();
+  }
 
-    unsigned char *signature = (unsigned char*)malloc(signatureLen);
-    if (!signature) {
-        perror("Memory allocation failed");
-        return 1;
-    }
+  unsigned char *signature = (unsigned char *)malloc(signatureLen);
+  if (!signature) {
+    perror("Memory allocation failed");
+    return 1;
+  }
 
-    if (EVP_DigestSignFinal(ctx, signature, &signatureLen) != 1) {
-        handleErrors();
-    }
+  if (EVP_DigestSignFinal(ctx, signature, &signatureLen) != 1) {
+    handleErrors();
+  }
 
-    // Print the signature in hexadecimal format
-    printf("Signature: \n");
-    std::string signature_base64 = base64_encode(signature, signatureLen);
-    printf("%s\n", signature_base64.c_str());
+  // Print the signature in hexadecimal format
+  printf("Signature: \n");
+  // std::string signature_base64 = base64_encode(signature, signatureLen);
+  std::string signature_base64 = hex_encode(signature, signatureLen);
+  printf("%s\n", signature_base64.c_str());
 
-    // Clean up
-    free(signature);
-    EVP_MD_CTX_destroy(ctx);
-    EVP_PKEY_free(privateKey);
+  // Clean up
+  free(signature);
+  EVP_MD_CTX_destroy(ctx);
+  EVP_PKEY_free(privateKey);
 
-    return 0;
+  return 0;
 }
 
 int main(int argc, char **argv) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <private key path> <message>\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-    const char *message = argv[2];
-    const char *privateKeyPath = argv[1];
+  if (argc != 3) {
+    fprintf(stderr, "Usage: %s <private key path> <message>\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+  const char *message = argv[2];
+  const char *privateKeyPath = argv[1];
 
-    if (signMessage(message, privateKeyPath) != 0) {
-        fprintf(stderr, "Signing failed\n");
-        return EXIT_FAILURE;
-    }
+  if (signMessage(message, privateKeyPath) != 0) {
+    fprintf(stderr, "Signing failed\n");
+    return EXIT_FAILURE;
+  }
 
-    return 0;
+  return 0;
 }
 
-std::string base64_encode(unsigned char* bytes, size_t length)
-{
-    BIO *bio, *b64;
-    BUF_MEM *bufferPtr;
+std::string base64_encode(unsigned char *bytes, size_t length) {
+  BIO *bio, *b64;
+  BUF_MEM *bufferPtr;
 
-    b64 = BIO_new(BIO_f_base64());
-    bio = BIO_new(BIO_s_mem());
-    bio = BIO_push(b64, bio);
+  b64 = BIO_new(BIO_f_base64());
+  bio = BIO_new(BIO_s_mem());
+  bio = BIO_push(b64, bio);
 
-    BIO_write(bio, bytes, length);
-    BIO_flush(bio);
-    BIO_get_mem_ptr(bio, &bufferPtr);
+  BIO_write(bio, bytes, length);
+  BIO_flush(bio);
+  BIO_get_mem_ptr(bio, &bufferPtr);
 
-    std::string result(bufferPtr->data, bufferPtr->length);
+  std::string result(bufferPtr->data, bufferPtr->length);
 
-    BIO_free_all(bio);
+  BIO_free_all(bio);
 
-    return result;
+  return result;
 }
